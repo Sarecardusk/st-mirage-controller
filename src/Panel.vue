406318e4a56117b0b2efbaac5e2ce7e2fe828be5
Sarecardusk <samsara_dusk@163.com>
2025-10-06 12:39:02 +0800

    feat(ui): Use header style, more unified with top-bar

--- src/Panel.vue
+++ src/Panel.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="example-extension-settings">
-    <div class="inline-drawer-header">
-      <b>{{ t`插件示例` }}</b>
+    <!-- 标准 top-bar 头部样式 -->
+    <div class="flex-container alignItemsBaseline wide100p">
+      <div class="flex1 flex-container alignItemsBaseline">
+        <h3 class="margin0">
+          <span data-i18n="Mirage 控制区">{{ t`Mirage 控制区` }}</span>
+          <a
+            href="https://github.com/Sarecardusk/st-mirage-controller?tab=readme-ov-file#st-mirage-controller"
+            target="_blank"
+          >
+            <span class="fa-solid fa-circle-question note-link-span"></span>
+          </a>
+        </h3>
+      </div>
+      <div class="flex-container">
+        <!-- 右侧功能按钮区域（可根据需要添加） -->
+      </div>
     </div>
+
+    <!-- 原有内容区域 -->
     <div class="example-extension_block flex-container">
       <input class="menu_button" type="submit" :value="t`示例按钮`" @click="handle_button_click" />
     </div>
