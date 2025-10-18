fe4f9978e71572ea50d46aaba75f8b8c1e971642
Sarecardusk <samsara_dusk@163.com>
2025-10-18 16:06:17 +0800

    feat(ui): Use header style, more unified with top-bar

--- src/Panel.vue
+++ src/Panel.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="example-extension-settings">
-    <div class="inline-drawer">
-      <div class="inline-drawer-toggle inline-drawer-header">
-        <b>{{ t`插件示例` }}</b>
-        <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
-      </div>
-      <div class="inline-drawer-content">
-        <div class="example-extension_block flex-container">
-          <input class="menu_button" type="submit" :value="t`示例按钮`" @click="handle_button_click" />
-        </div>
-
-        <div class="example-extension_block flex-container">
-          <input v-model="settings.button_selected" type="checkbox" />
-          <label for="example_setting">{{ t`示例开关` }}</label>
-        </div>
-
-        <hr class="sysHR" />
-
-        <!-- Router View for future pages -->
-        <router-view />
+  <div class="mirage-controller-container flex-container">
+    <div class="alignitemscenter flex-container wide100p">
+      <h3 class="margin0 flex1">
+        <span>{{ t`Mirage Controller` }}</span>
+      </h3>
+      <div class="menu_button menu_button_icon interactable" tabindex="0" role="button" @click="handle_button_click">
+        <i class="fa-solid fa-diagram-project"></i>
+        <span>{{ t`示例按钮` }}</span>
       </div>
     </div>
+
+    <hr class="wide100p margin0" />
+
+    <div class="alignitemsflexstart flex-container wide100p">
+      <div class="flex-container alignitemscenter">
+        <label for="mirage_example_setting" class="checkbox_label flexNoGap">
+          <input id="mirage_example_setting" v-model="settings.button_selected" type="checkbox" />
+          <span>{{ t`示例开关` }}</span>
+        </label>
+      </div>
+    </div>
+
+    <!-- Router View for future pages -->
+    <router-view />
   </div>
 </template>
 
--- src/panel.ts
+++ src/panel.ts
@@ -21,6 +21,56 @@ const i18n = {
 app.use(i18n);
 
 export function initPanel() {
-  const $app = $('<div id="tavern_extension_example">').appendTo('#extensions_settings2');
+  // 创建顶栏 drawer 结构（包含 interactable 类，但不包含 openIcon）
+  const drawerHtml = `
+    <div id="mirage-controller-button" class="drawer">
+      <div class="drawer-toggle">
+        <div class="drawer-icon fa-solid fa-diagram-project fa-fw interactable"
+             title="Mirage Controller"
+             data-i18n="[title]Mirage Controller"
+             tabindex="0"
+             role="button"></div>
+      </div>
+      <div id="mirage-controller-panel" class="drawer-content">
+        <div id="mirage-controller-app"></div>
+      </div>
+    </div>
+  `;
+
+  // 将 drawer 插入到 Extensions 后
+  const $drawer = $(drawerHtml);
+  $drawer.insertAfter('#extensions-settings-button');
+
+  const $toggle = $drawer.find('.drawer-toggle');
+  const $icon = $drawer.find('.drawer-icon');
+  const $content = $drawer.find('.drawer-content');
+
+  // 添加点击事件来切换面板的展开/收起
+  $toggle.on('click', function (e) {
+    e.stopPropagation();
+
+    // 关闭其他所有 drawer
+    $('.drawer')
+      .not($drawer)
+      .each(function () {
+        $(this).find('.drawer-content').removeClass('openDrawer');
+        $(this).find('.drawer-icon').removeClass('openIcon');
+      });
+
+    // 切换当前 drawer
+    $content.toggleClass('openDrawer');
+    $icon.toggleClass('openIcon');
+  });
+
+  // 监听其他 drawer 的点击，自动关闭当前 drawer
+  $(document).on('click', '.drawer-toggle', function () {
+    if (!$(this).closest('#mirage-controller-button').length) {
+      $content.removeClass('openDrawer');
+      $icon.removeClass('openIcon');
+    }
+  });
+
+  // 挂载 Vue 应用到 drawer 内容区
+  const $app = $('#mirage-controller-app');
   app.mount($app[0]);
 }
