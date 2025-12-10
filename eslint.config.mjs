eaddbae59144e17363bb309c9f84b6eaa426b338
Sarecardusk <@>
2025-12-10 17:02:47 +0800

    chore: 彻底移除 tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
 import importx from 'eslint-plugin-import-x';
 import pinia from 'eslint-plugin-pinia';
 import vue from 'eslint-plugin-vue';
@@ -18,23 +17,6 @@ export default [
   importx.flatConfigs.typescript,
   ...vue.configs['flat/recommended'],
   pinia.configs['recommended-flat'],
-  {
-    plugins: {
-      'better-tailwindcss': eslintPluginBetterTailwindcss,
-    },
-    rules: {
-      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
-      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
-      'better-tailwindcss/enforce-consistent-line-wrapping': ['off', { printWidth: 120 }],
-      'better-tailwindcss/no-unregistered-classes': ['off', { ignore: ['fa-*'] }],
-    },
-    settings: {
-      'better-tailwindcss': {
-        entryPoint: 'src/global.css',
-        tailwindConfig: 'tailwind.config.js',
-      },
-    },
-  },
   {
     settings: {
       'import-x/resolver': {
@@ -85,5 +67,5 @@ export default [
     },
   },
   eslintConfigPrettier,
-  globalIgnores(['dist/**', 'node_modules/**', 'eslint.config.mjs', 'postcss.config.js', 'vite.config.ts', '.temp/**']),
+  globalIgnores(['dist/**', 'node_modules/**', 'eslint.config.mjs', 'vite.config.ts', '.temp/**']),
 ];
